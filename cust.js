3d82a9691b0ec03128c86b5527353a74ac7d4382
Alaa2455 <53766757+Alaa2455@users.noreply.github.com>
2019-09-07 01:28:01 +0300

    Add files via upload

--- cust.js
+++ cust.js
@@ -1,4 +1,13 @@
 
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("btnScroll").style.display = "block";
+
+    } else {
+        document.getElementById("btnScroll").style.display = "none";
+    }
+}
+
 function toUp() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
